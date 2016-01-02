ff806b30a8d1f5887cdf909824f9ddc085c11c2b
ntta <nguyen-tran.tuananh@outlook.com>
2016-01-03 04:03:01 +0700

    Fix important bug

--- quadtree.py
+++ quadtree.py
@@ -3,6 +3,8 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+import random
+import time
 
 class Node:
     children = None
@@ -10,46 +12,40 @@ class Node:
     center_of_mass = None
     bbox = None
 
+theta = 0.5
 G = 6.67408e-11 #m^3 kg^-1 s^-2
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal')
 
 def quad_insert(root, x, y, m):
-    new_particle = Node()
-    new_particle.mass = m
-    new_particle.center_of_mass = [x,y]
     if root.mass is None:   #when the root is empty, add the first particle
         root.mass = m
         root.center_of_mass = [x,y]
+        return
     elif root.children is None:
         root.children = [None,None,None,None]
-        #add the old particle in a children
         old_quadrant = quadrant_of_particle(root.bbox, root.center_of_mass[0], root.center_of_mass[1])
-        old_particle = Node()
-        old_particle.mass = root.mass
-        old_particle.center_of_mass = [root.center_of_mass[0], root.center_of_mass[1]]
-        old_particle.bbox = quadrants(root.bbox,old_quadrant)
-        root.children[old_quadrant] = old_particle
-        #Put the new particle in the children which is the same level with the old one
+        if root.children[old_quadrant] is None:
+            root.children[old_quadrant] = Node()
+            root.children[old_quadrant].bbox = quadrants(root.bbox,old_quadrant)
+        quad_insert(root.children[old_quadrant], root.center_of_mass[0], root.center_of_mass[1], root.mass)
         new_quadrant = quadrant_of_particle(root.bbox, x, y)
         if root.children[new_quadrant] is None:
-            new_particle.bbox = quadrants(root.bbox,new_quadrant)
-            root.children[new_quadrant] = new_particle
-        else:
-            quad_insert(root.children[new_quadrant], x, y, m)
-        root.mass = root.mass + new_particle.mass
-        root.center_of_mass[0] = (old_particle.center_of_mass[0]*old_particle.mass + new_particle.center_of_mass[0]*new_particle.mass) / root.mass
-        root.center_of_mass[1] = (old_particle.center_of_mass[1]*old_particle.mass + new_particle.center_of_mass[1]*new_particle.mass) / root.mass
+            root.children[new_quadrant] = Node()
+            root.children[new_quadrant].bbox = quadrants(root.bbox,new_quadrant)
+        quad_insert(root.children[new_quadrant], x, y, m)
+        root.center_of_mass[0] = (root.center_of_mass[0]*root.mass + x*m) / (root.mass + m)
+        root.center_of_mass[1] = (root.center_of_mass[1]*root.mass + y*m) / (root.mass + m)
+        root.mass = root.mass + m
     else:
         new_quadrant = quadrant_of_particle(root.bbox, x, y)
         if root.children[new_quadrant] is None:
-            new_particle.bbox = quadrants(root.bbox, new_quadrant)
-            root.children[new_quadrant] = new_particle
-        else:
-            quad_insert(root.children[new_quadrant], x, y, m)
-        root.center_of_mass[0] = (root.center_of_mass[0] * root.mass + new_particle.center_of_mass[0] * new_particle.mass) / (root.mass + new_particle.mass)
-        root.center_of_mass[1] = (root.center_of_mass[1] * root.mass + new_particle.center_of_mass[1] * new_particle.mass) / (root.mass + new_particle.mass)
-        root.mass = root.mass + new_particle.mass
+            root.children[new_quadrant] = Node()
+            root.children[new_quadrant].bbox = quadrants(root.bbox, new_quadrant)
+        quad_insert(root.children[new_quadrant], x, y, m)
+        root.center_of_mass[0] = (root.center_of_mass[0]*root.mass + x*m) / (root.mass + m)
+        root.center_of_mass[1] = (root.center_of_mass[1]*root.mass + y*m) / (root.mass + m)
+        root.mass = root.mass + m
 
 def display(root):
     if root.mass is None:
@@ -68,13 +64,27 @@ def display(root):
     else:
         plt.scatter(root.center_of_mass[0], root.center_of_mass[1])
 
+def compute_force(root,x,y,m):
+    f = 0
+    r = distance(x, y, root.center_of_mass[0], root.center_of_mass[1])
+    d = root.bbox[1]-root.bbox[0]
+    if root.mass is None or r == 0:
+        return 0
+    elif d/r <= theta or root.children is None:
+        f = f + force(x, y, m, root.center_of_mass[0], root.center_of_mass[1], root.mass)
+    else:
+        for i in xrange(4):
+            if root.children[i] is not None:
+                f = f + compute_force(root.children[i], x, y, m)
+    return f
+
 ################################################# SUPPORTING FUNCTION ##############################################################
 
 def force(x, y, m, xcm, ycm, mcm):
-    r = math.sqrt((xcm-x)**2+(ycm-y)**2)
-    xf = G*m*mcm*(xcm-x)/r**3
-    yf = G*m*mcm*(ycm-y)/r**3
-    return [xf, yf]
+    return (G*m*mcm)/(distance(x,y,xcm,ycm)**2)
+
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2-x1)**2+(y2-y1)**2)
 
 def plt_node(x, y, width):
     ax.add_patch(patches.Rectangle((x, y), width, width, fill = False))
@@ -82,21 +92,11 @@ def plt_node(x, y, width):
 def find_root_bbox(array):
     """ Create a suitable square boundary box for the input particles
     """
-    xmin = array[0][0]
-    xmax = array[0][0]
-    ymin = array[0][1]
-    ymax = array[0][1]
     if len(array) == 0 or len(array) == 1:
         return None
-    for i in xrange(len(array)):
-        if array[i][0] < xmin:
-            xmin = array[i][0]
-        if array[i][0] > xmax:
-            xmax = array[i][0]
-        if array[i][1] < ymin:
-            ymin = array[i][1]
-        if array[i][1] > ymax:
-            ymax = array[i][1]
+    bmin = np.min(array, axis = 0)
+    bmax = np.max(array, axis = 0)
+    xmin, xmax, ymin, ymax = bmin[0], bmax[0], bmin[1], bmax[1]
     if xmax - xmin == ymax - ymin:
         return xmin, xmax, ymin, ymax
     elif xmax - xmin > ymax - ymin:
@@ -145,9 +145,17 @@ def data_from_file(filename, array):
                 x,y,m = line.split(',')
                 array.append([float(x),float(y),float(m)])
 
+def data_from_random(quantity, array):
+    for i in xrange(quantity):
+        x = random.randrange(0, 10000)
+        y = random.randrange(0, 10000)
+        m = random.randrange(100, 10000)
+        array.append([x, y, m])
+
 if __name__ == '__main__':
     filename = ('testcase.txt')
     particles = []
+    #data_from_random(10000, particles)
     data_from_file(filename, particles)
     root = Node()
     root.center_of_mass = []
@@ -159,4 +167,6 @@ if __name__ == '__main__':
     print 'Coordinate of center of mass: ',root.center_of_mass
     plt.scatter(root.center_of_mass[0], root.center_of_mass[1], c='r', marker='x', s=50)
     display(root)
+    print 'Theta: ', theta
+    print 'Force interact on particle [0,0,100]:', compute_force(root, 0, 0, 100)
     plt.show()
